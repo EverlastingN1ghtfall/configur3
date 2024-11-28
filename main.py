de6986a72f80ea23d27bdf470191e4f76fc3a30f
_D1zast3R_ <petru00000@mail.ru>
2024-11-28 20:20:40 +0300

    new test for mases mas_handler works todo: content_handler (consts calculating, dicts)

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def check_name(name: str) -> bool:
 def mas_split(line: str) -> list:
     counts = {
         "Strs": 0,
-        "Mases": -1
+        "Mases": 0
     }
+    if line[0] == '{':
+        counts["Mases"] -= 1
     buff = ""
     out = []
     for i in line:
@@ -49,6 +51,9 @@ class Solution:
         if content[-1] != '"':
             err = f'''Invalid syntax. Missing closing ". Line {ind+1}: '{self.data[ind]}' '''
             crash_handler(err)
+        if content.count('"') > 2:
+            err = f'''Invalid syntax. Too many ". Line {ind+1}: '{self.data[ind]}' '''
+            crash_handler(err)
         return True
 
     def check_numeral(self, content: str, ind: int) -> bool:
@@ -57,24 +62,30 @@ class Solution:
             crash_handler(err)
         return True
 
-
-
-
     def mas_handler(self, add_to: list, content: str, ind: int) -> int:
-        contains = content.split(',')
+        contains = mas_split(content)
+        contains[0] = contains[0][1:]
         i = 0
         cur_line = ind
         while i < len(contains):
             contains[i] = contains[i].lstrip(' ').rstrip(' ')
-            new_line = self.content_handler(add_to, contains[i], cur_line, "")
-            if contains[i][0] == '{':
-                closing_ind = -1
-                for j in range(i, len(contains)):
-                    if contains[j].find
-            elif contains[i][0] == '$':
+            if i == len(contains) - 1:
+                new_line = self.content_handler(add_to, contains[i][:-1], cur_line, "")
+            else:
+                new_line = self.content_handler(add_to, contains[i], cur_line, "")
+            if cur_line != new_line:
+                i = 0
+                cur_line = new_line
+                contains = mas_split(self.data[new_line])
+            i += 1
+
+        if contains[-1][-1] != '}':
+            err = "Syntax error. Could not find }" + f". Line {cur_line+1}: '{self.data[cur_line]}'"
+            crash_handler(err)
+        return cur_line
 
 
-    def comment_handler(self, ind: int) -> int:
+    def comment_handler(self, ind: int) -> int: # returns line index where comment ends
         comment = []
         for i in range(ind + 1, len(self.data)):
             if "-->" in self.data[i]:
@@ -91,7 +102,6 @@ class Solution:
         crash_handler(err)
 
     def content_handler(self, add_to: list, content: str, ind: int, name: str) -> int:
-        name = name.lstrip(' ').rstrip(' ')
         if name != "" and not check_name(name):
             err = f"Invalid variable name (line {ind+1}): '{name}'"
             crash_handler(err)
@@ -99,44 +109,48 @@ class Solution:
         if content[0] == '@':
             if self.check_string(content, ind):
                 content = content[2:-1]
-                if name != "":
-                    add_to.append({"type": "str", "name": name, "content": content})
-                else:
-                    add_to.append({"type": "str", "content": content})
+
+                add_to.append({"type": "str", "name": name, "content": content})
                 return ind
         elif content[0].isdigit():
             if self.check_numeral(content, ind):
-                if name != "":
-                    add_to.append({"type": "int", "name": name, "content": int(content)})
-                else:
-                    add_to.append({"type": "int", "content": int(content)})
+                add_to.append({"type": "int", "name": name, "content": int(content)})
                 return ind
         elif content[0] == '{':
             add_to.append({"type": "mas", "name": name, "content": []})
 
             ind_end = self.mas_handler(add_to[-1]["content"], content, ind)
             return ind_end
+        else:
+            return ind
 
     def reader(self) -> None:
         i = 0
         while i < len(self.data):
             # print(self.data[i])
-            if self.data[i] == "<!--":
-                i = self.comment_handler(i)
-            elif self.data[i][0].isalpha():
-                line = self.data[i]
-                col_ind = line.find(':')
-                if col_ind == -1:
-                    err = f"Invalid syntax. Should be assignment, but : was not found. Line {i+1}: '{line}'"
-                    crash_handler(err)
+            if self.data[i] != "":
+                if self.data[i] == "<!--":
+                    i = self.comment_handler(i)
+                elif self.data[i][0].isalpha():
+                    line = self.data[i]
+                    col_ind = line.find(':')
+                    if col_ind == -1:
+                        err = f"Invalid syntax. Should be assignment, but : was not found. Line {i+1}: '{line}'"
+                        crash_handler(err)
 
-                name, content = line.split(':')
-                self.content_handler(self.vars, content, i, name)
+                    name, content = line.split(':')
+                    name = name.rstrip(' ')
+                    if name == "":
+                        err = f"Naming error. Name can not be empty. Line {i+1}: '{self.data[i]}'"
+                        crash_handler(err)
+
+                    i = self.content_handler(self.vars, content, i, name)
+            # print(type(i), i)
             i += 1
 
 
 if __name__ == "__main__":
-    path = "test_file.txt"
+    path = "test_hard_mases.txt"
     with open(path, 'r', encoding='utf-8') as f:
         data = f.read()
 
